ba22a1aeffa087b1e14b902d882ce1a71c63bfb6
satoshi-shopify <akustot1201@gmail.com>
2024-02-04 10:34:03 +0900

    footerの途中まで完了

---
+++ src/js/js-hamburger.js
@@ -0,0 +1,15 @@
+
+jQuery(function ($) {
+
+  //ナビバートグル
+  $('.js-hamburger').on('click', function () {
+    if ($('.js-hamburger').hasClass('is-open')) {
+      $('.js-drawer-menu').fadeOut();
+      $(this).removeClass('is-open');
+    } else {
+      $('.js-drawer-menu').fadeIn();
+      $(this).addClass('is-open');
+    }
+  });
+
+});
