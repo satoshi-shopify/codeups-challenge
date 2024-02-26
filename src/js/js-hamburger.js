fabaa52197ab6a030c5b32ed609608e093f3eb29
satoshi-shopify <akustot1201@gmail.com>
2024-02-27 07:22:58 +0900

    ドロワーメニューを画面幅768以上になると非表示にする

--- src/js/js-hamburger.js
+++ src/js/js-hamburger.js
@@ -22,4 +22,22 @@ jQuery(function ($) {
     }
   });
 
+  // ウィンドウの幅を検知してクラスを操作する関数
+  function toggleDrawerMenu() {
+    var drawerMenu = $(".js-drawer-menu");
+    
+    // ウィンドウの幅が一定以上の場合にクラスを追加・削除する
+    // 上段では、画面幅768px以上でドロワーメニューを非表示にする、フェードアウトする
+    // 下段では、どうでない時ハンバーガーメニューからis-openクラスを除く。
+    if (window.innerWidth >= 768) {
+      $('.js-drawer-menu').fadeOut();
+    } else {
+      $('.js-hamburger').removeClass('is-open')
+    }
+  }
+
+  // ページ読み込み時とウィンドウリサイズ時に切り替え関数を実行
+  $(document).ready(toggleDrawerMenu);
+  $(window).resize(toggleDrawerMenu);
+
 });
