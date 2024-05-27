7ca9c602ca5e5a71dff388dd1b64b47560de0f07
satoshi-shopify <akustot1201@gmail.com>
2024-05-27 16:44:43 +0900

    modalの実装完了

--- src/js/script.js
+++ src/js/script.js
@@ -143,3 +143,16 @@ progressbar：スライドの進捗に応じてプログレスバーが伸びる
 custom：自由にカスタマイズ
 
 =====================================================*/
+
+// Modaal js
+//3. 画像のモーダル
+$(".js-modal-img").modaal({
+	type: 'image',
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
